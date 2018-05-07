7edbf155acbfaad3d6281f7887edc338e6d4caef
MohammadJozaa7 <36266902+MohammadJozaa7@users.noreply.github.com>
2018-05-07 22:18:02 +0300

    First Reverse

--- Q+A.js
+++ Q+A.js
@@ -7,6 +7,24 @@ function a(){
 a({a:1},'asd');
 /////////////////////////////////////////////////////////////////////
 
+function FirstReverse(str) { 
+  var result='';
+  for (var i = str.length-1; i > -1; i--) {
+    result+=str[i]
+  }
+  // code goes here  
+  return result; 
+         
+}
+   
+///////////////////////////////////////////////////////
+
+                        
 
 
+function FirstReverse2(str) { 
+  
+  return str.split("").reverse().join(""); 
+         
+}
 
