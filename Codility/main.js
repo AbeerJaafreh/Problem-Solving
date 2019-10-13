316b9fbf48bf0c4464a32c8972287332dc070212
MohammadJozaa7 <m.jozaa@yahoo.com>
2019-10-13 23:23:16 +0300

    unpair 1

--- Codility/main.js
+++ Codility/main.js
@@ -1,9 +1,26 @@
 console.log('Codility');
+function unpairNumber(A) {
+  // write your code in JavaScript (Node.js 8.9.4)
+  let obj = {};
+  for (const iterator of A) {
+    console.log(iterator);
+    if (!!obj[iterator]) {
+      obj[iterator] += 1;
+    } else {
+      obj[iterator] = 1;
+    }
+  }
+  console.log(obj);
+  for (const iterator of A) {
+    if (obj[iterator] === 1) {
+      return iterator;
+    }
+  }
+}
 
-
-console.log('r:', solution(9));
+console.log('r:', unpairNumber([9, 9, 9, 201, 0, 201]));
 //2
-console.log('r:', solution(529));
+// console.log('r:', solution(529));
 //4
-console.log('r:', solution(20));
+// console.log('r:', solution(20));
 //2
